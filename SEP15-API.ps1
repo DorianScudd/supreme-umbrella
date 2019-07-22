1f7f6079e32b7d4f4b451da4c8f18f7899782c03
DorianScudd <35775843+DorianScudd@users.noreply.github.com>
2019-07-22 08:40:39 -0400

    Update SEP15-API.ps1

--- SEP15-API.ps1
+++ SEP15-API.ps1
@@ -1,23 +1,60 @@
-#Working through learning to authenticate and query Symantec REST API for Symantec 15
+# Get Authorization Token
+$user = '<client_id>'
+$pass = '<client_secret>'
 
-# Force TLS 1.2
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+$cred = [Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($user + ':' + $pass))
 
-$baseURI = "usea1.r3.securitycloud.symantec.com"
-$basePath = "https://${baseURI}/r3_epmp_i/"
+$uri = 'https://usea1.r3.securitycloud.symantec.com/r3_epmp_i'
 
-$CIDCs = '<CI AND DC>'
-$CIDCs64 = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($CIDCs))
-$cToken = 'oauth2/tokens'
-$bearer = 'Bearer'+$CIDCs64
+$path = '/oauth2/tokens'
+$params = '?grant_type=client_credentials'
+$method = 'Post'
 
 $headers = @{
-    'Accept' = 'application/json'
-    'Authorization' = $bearer
-    'Content-Type' = 'application/x-www-form-urlencoded'
-    'grant_type' = 'client_credentials&scope=customer'
+'Authorization' = "Basic $cred";
+'Accept' = 'application/json';
+'Content-Type' = 'application/x-www-form-urlencoded';
 }
 
-#$result = Invoke-RestMethod -Method Get -Uri $basePath$cToken -Headers $headers #-ContentType 'application/json' 
+$auth = Invoke-RestMethod -Uri $uri$path$params -Method $method -Headers $headers
 
-$result = Invoke-RestMethod -Method Get -Uri "https://usea1.r3.securitycloud.symantec.com/r3_epmp_i/oauth2/tokens" -Headers $headers
+# Export Events
+$path = '/sccs/v1/events/export'
+$params = ''
+$method = 'Post'
+
+$headers = @{
+'Authorization' = $auth.access_token;
+'Content-Type' = 'application/json';
+'x-epmp-customer-id' = 'customer_id';
+'x-epmp-domain-id' = '<domain_id';
+'x-epmp-product' = 'SAEP';
+}
+
+$request_info = @{
+'batchSize' = '10000';
+#Different types of events to capture
+
+#'type' = 'APP ISOLATION';
+#'type' = 'EXPLOIT PROTECTION';
+#'type' = 'FIREWALL';
+'type' = 'MALWARE PROTECTION';
+#'type' = 'NETWORK IPS';
+#'type' = 'BEHAVIORAL ANALYSIS';
+#'type' = 'APP CONTROL';
+#'type' = 'DECEPTION';
+'startDate' = '2019-07-21T00:00:00.000Z';
+'endDate' = Get-Date -UFormat "%Y-%m-%dT%H:%M:%S.000Z";
+
+#Per SEP: "You continually invoke the Events API command until an empty set is returned.
+#   You use the same parameters. To increase the amount of data returned from a single command, 
+#   change the value of the batchSize parameter."
+}
+
+$body = $request_info | ConvertTo-Json -Compress
+
+echo $body
+
+$events = Invoke-RestMethod -Uri $uri$path$params -Method $method -Headers $headers -Body $body
+
+echo $events
